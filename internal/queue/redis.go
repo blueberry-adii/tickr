43ee500eba66b06f61b3582e0ca331b359eae8a6
Aditya <adithyap455@proton.me>
2026-01-09 19:07:36 +0530

    feat: implement dequeue for jobs from redis queue

--- internal/queue/redis.go
+++ internal/queue/redis.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"github.com/blueberry-adii/tickr/internal/jobs"
 	"github.com/go-redis/redis/v8"
@@ -37,4 +38,27 @@ func (q *RedisQueue) getQueueKey(priority int) string {
 	} else {
 		return "ticker:queue:low"
 	}
+}
+
+func (q *RedisQueue) Dequeue(ctx context.Context) (*jobs.Job, error) {
+	queues := []string{
+		"tickr:queue:high",
+		"tickr:queue:medium",
+		"ticker:queue:low",
+	}
+
+	res, err := q.client.BRPop(ctx, time.Second*5, queues...).Result()
+
+	if err == redis.Nil {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	var job jobs.Job
+	if err := json.Unmarshal([]byte(res[1]), &job); err != nil {
+		return nil, err
+	}
+	return &job, nil
 }
