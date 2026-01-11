cb5c398bdc2b0f652284fd21893ddb748fa4ecf3
Aditya <adithyap455@proton.me>
2026-01-11 12:31:14 +0530

    docs: add comments in job model

--- internal/jobs/model.go
+++ internal/jobs/model.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+/*
+Job struct defines what fields each Job
+must have
+Payload is the data needed to execute the Job
+*/
 type Job struct {
 	ID        string          `json:"id"`
 	JobType   string          `json:"jobtype"`
