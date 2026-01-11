5e1cc0aa7ffdd6f9aa48cd421051e1d77d6c1f47
Aditya <adithyap455@proton.me>
2026-01-11 12:29:11 +0530

    docs: add comments in package api for better readability

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -10,6 +10,10 @@ import (
 	"github.com/google/uuid"
 )
 
+/*
+response struct: HTTP Response must always be
+returned in this format
+*/
 type response struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
@@ -17,16 +21,22 @@ type response struct {
 	Success bool   `json:"success"`
 }
 
+/*
+Handler Struct which depends on scheduler through
+dependency injection
+*/
 type Handler struct {
 	scheduler *scheduler.Scheduler
 }
 
+/*Handler Constructor*/
 func NewHandler(s *scheduler.Scheduler) *Handler {
 	return &Handler{
 		scheduler: s,
 	}
 }
 
+/*API Health Endpoint Handler*/
 func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
@@ -39,18 +49,22 @@ func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+/*API Endpoint to submit jobs*/
 func (h *Handler) SubmitJob(w http.ResponseWriter, r *http.Request) {
+	/*http request body struct*/
 	var body struct {
 		JobType string          `json:"jobtype"`
 		Payload json.RawMessage `json:"payload"`
 		Delay   int             `json:"delay"`
 	}
 
+	/*Decode JSON Req Body into variable body*/
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		http.Error(w, "Invalid Body Format", http.StatusBadRequest)
 		return
 	}
 
+	/*Create a New Job with the given data from request body*/
 	job := jobs.Job{
 		ID:        uuid.NewString(),
 		JobType:   body.JobType,
@@ -59,11 +73,21 @@ func (h *Handler) SubmitJob(w http.ResponseWriter, r *http.Request) {
 		CreatedAt: time.Now(),
 	}
 
+	/*
+		If job is scheduled/given delay, push the job into waiting queue,
+		otherwise push into ready queue
+	*/
 	if body.Delay > 0 {
 		h.scheduler.PushWaitingQueue(r.Context(), &job, body.Delay)
 	} else {
 		h.scheduler.PushReadyQueue(r.Context(), &job)
 	}
+
+	/*
+		Set response content type into application json
+		response status code - 200 (OK)
+		encode Go struct and send response in json format
+	*/
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response{
--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -6,6 +6,18 @@ import (
 	"time"
 )
 
+/*
+Logging Middleware
+This is responsible for logging all the HTTP requests
+received/listened by the server
+
+example:
+POST /health 2.033ms
+
+It accepts a handler function in parameters,
+performs middleware logic, and servers the handler function
+and returns the handler wrapped with middleware logic
+*/
 func Logging(next http.HandlerFunc) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
