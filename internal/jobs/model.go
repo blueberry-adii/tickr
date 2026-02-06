c8b58cef9fce0115913b57d4af49ccd99e199335
Aditya <adithyap455@proton.me>
2026-02-06 23:05:01 +0530

    refactor: separate job structs for mysql and redis

--- internal/jobs/model.go
+++ internal/jobs/model.go
@@ -7,15 +7,29 @@ import (
 	"github.com/blueberry-adii/tickr/internal/enums"
 )
 
+type RedisJob struct {
+	JobID int64 `json:"job_id"`
+}
+
 /*
 Job struct defines what fields each Job
 must have
 Payload is the data needed to execute the Job
 */
 type Job struct {
-	ID        int64           `json:"id"`
-	JobType   string          `json:"jobtype"`
-	Payload   json.RawMessage `json:"payload"`
-	Status    enums.Status    `json:"status"`
-	CreatedAt time.Time       `json:"createdAt"`
+	ID      int64           `json:"id"`
+	JobType string          `json:"jobtype"`
+	Payload json.RawMessage `json:"payload"`
+
+	Status      enums.Status `json:"status"`
+	Attempt     int          `json:"attempt"`
+	MaxAttempts int          `json:"maxAttempts"`
+
+	ScheduledAt time.Time  `json:"scheduledAt"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	StartedAt   *time.Time `json:"startedAt"`
+	FinishedAt  *time.Time `json:"finishedAt"`
+
+	LastError *string `json:"lastError"`
+	WorkerID  *string `json:"workerID"`
 }
